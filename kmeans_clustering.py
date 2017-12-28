5e7a694da39f9ef132c49bec92f25bc8dc556140
aj002 <ajain38@rocketmail.com>
2017-12-28 21:31:30 +0530

    kmeans clustering

--- kmeans_clustering.py
+++ kmeans_clustering.py
@@ -13,4 +13,17 @@ X = dataset.iloc[:, [3, 4]].values
 from sklearn.cluster import KMeans
 wcss = []
 for i in range(1,11):
-    kmeans = KMeans(n_cluster = i, init = 'k-means++', max_)
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1,11), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of Clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+#Applying k-means to the dataset
+kmeans = KMeans(n_clusters = 5, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
+y_kmeans = kmeans.fit_predict(X)
+
+#Visualizing the clusters
